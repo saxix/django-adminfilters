223b87f3cbfc377aee3a6e67fc154d3186a46fe8
sax <s.apostolico@gmail.com>
2024-01-24 07:26:11 +0100

    updates RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Django AdminFilters'
-copyright = '2012-2022, Stefano Apostolico'
+copyright = '2012-2024, Stefano Apostolico'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -125,13 +125,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('READTHEDOCS', None) == 'True':
-    html_theme = 'default'
-else:
-    import sphinx_rtd_theme
-
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# if os.environ.get('READTHEDOCS', None) == 'True':
+#     html_theme = 'default'
+# else:
+#     import sphinx_rtd_theme
+#
+#     html_theme = 'sphinx_rtd_theme'
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 #
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
