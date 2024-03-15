ff90efaeccdef62a3b991190cb2ebe4a684aa3cd
Gio <g.bronzini@gmail.com>
2024-03-15 17:35:53 +0000

    Fixed tests

--- tests/functional/test_f_unionlistfilter.py
+++ tests/functional/test_f_unionlistfilter.py
@@ -23,7 +23,7 @@ def test_unionlist_filter(admin_factory):
     site = admin_factory('UnionFieldListFilters')
     site.wait_and_click(
         By.XPATH,
-        '//*[@id="changelist-filter"]/details/ul/li/a[contains(text(), "AC/DC")]'
+        '//*[@id="changelist-filter"]//a[contains(text(), "AC/DC")]'
     )
     site.wait_for(By.XPATH, '//*[@id="result_list"]/tbody/tr')
     assert len(
@@ -32,7 +32,7 @@ def test_unionlist_filter(admin_factory):
     site.wait_for(By.XPATH, '//*[@id="result_list"]/tbody/tr')
     site.wait_and_click(
         By.XPATH,
-        '//*[@id="changelist-filter"]/details/ul/li/a[contains(text(), "Abba")]'
+        '//*[@id="changelist-filter"]//a[contains(text(), "Abba")]'
     )
     assert len(
         site.driver.find_elements(By.XPATH, '//*[@id="result_list"]/tbody/tr')
